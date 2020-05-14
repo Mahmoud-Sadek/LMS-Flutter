b2a0892de9eb575cf18d0ee18faceecf35096ed1
Mkhira <45259648+Mkhira@users.noreply.github.com>
2020-05-14 04:34:11 +0200

    work

--- lib/services/SignUpService/RegisterApi.dart
+++ lib/services/SignUpService/RegisterApi.dart
@@ -1,7 +1,4 @@
 
-
-
-
 import 'package:http/http.dart' as http ;
 import 'package:work/Model/SignUPModel/CountryModel.dart';
 import 'package:work/Model/SignUPModel/GradeModel.dart';
@@ -15,40 +12,27 @@ import 'package:work/utils/common.dart';
 
 
 
-Future<RegisterModel> RegisterApi(
-{String mobile,
-  String emailAddress,
-  String fireBaseToken,
-  String gender,
-  String fullName,
-  String password,
-  String cityId,
-  String groupId,
-  String image,}
-    ) async {
+Future<String> RegisterApi(RegisterModel bodys) async {
   var headers= await Common.getHeaders();
+  var body = json.encode(bodys.toJson());
 
   var response = await http.post(Common.BaseURL+"api/Studnets/Create",
 
     headers: headers,
-    body: jsonEncode(<String, dynamic>{
-    'Mobile' : mobile,
-   'EmailAddress' :emailAddress,
-   'FireBaseToken': fireBaseToken,
-    'Gender': gender,
-    'FullName':fullName,
-    'Password':password,
-    'CityId':cityId,
-    'GroupId':groupId,
-    'Image':image,
-    }),
+    body: body,
   );
-  if (response.statusCode == 201) {
-    // If the server did return a 201 CREATED response,
+  if (response.statusCode == 200) {
+    // If the server did return a 200 CREATED response,
     // then parse the JSON.
     print(response.body);
-    return RegisterModel.fromJson(json.decode(response.body));
+    return response.body;
 
+  } else if (response.statusCode == 400) {
+    // If the server did return a 200 CREATED response,
+    // then parse the JSON.
+//    print(response.body);
+//    return RegisterModel.fromJson(json.decode(response.body));
+    throw Exception('Failed to Submit Data');
   } else {
     // If the server did not return a 201 CREATED response,
     // then throw an exception.
