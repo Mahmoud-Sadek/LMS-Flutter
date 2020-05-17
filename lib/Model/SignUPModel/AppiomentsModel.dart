6beb5e1ea9cce3a7bd820df1a37f0a96c300a196
Mkhira <45259648+Mkhira@users.noreply.github.com>
2020-05-18 00:31:53 +0200

    work

--- lib/Model/SignUPModel/AppiomentsModel.dart
+++ lib/Model/SignUPModel/AppiomentsModel.dart
@@ -1,16 +1,16 @@
 
 
 
-class AppiomentsModel{
+class AppointmentModel{
 
 
   String day;
   String to;
   String from;
 
-  AppiomentsModel({this.day, this.to, this.from});
+  AppointmentModel({this.day, this.to, this.from});
 
-  AppiomentsModel.fromJson(Map<String, dynamic> json) {
+  AppointmentModel.fromJson(Map<String, dynamic> json) {
     day = json['Day'];
     to = json['To'];
     from = json['From'];
